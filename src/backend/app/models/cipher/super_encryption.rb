401815a67cbb09f1205ed781410197fc8f8baa9f
FOS-Intern <intern@fata-organa.com>
2024-02-22 12:09:27 +0700

    add super encryption for decryption

--- src/backend/app/models/cipher/super_encryption.rb
+++ src/backend/app/models/cipher/super_encryption.rb
@@ -3,11 +3,9 @@ class Cipher::SuperEncryption
     def self.encrypt1(key_trans,key_subs,plaintext)
         encrypt1_letter = Cipher::ExtendedVigenere.encrypt(key_subs,plaintext)
         encrypt1_length = encrypt1_letter.length
-        length_row = 0
-        if(encrypt1_length%key_trans == 0) 
-            length_row = encrypt1_length/key_trans
-        else 
-            length_row = encrypt1_length/key_trans + 1
+        length_row = encrypt1_length/key_trans
+        if(encrypt1_length%key_trans != 0) 
+            length_row += 1
         end
         ret = ""
         for column_ind in 0..(key_trans-1) do
@@ -21,8 +19,25 @@ class Cipher::SuperEncryption
         return ret
     end
 
-    def self.decrypt1(key_trans,key_subs,decrypt)
-        decrypt1_letter = Cipher::ExtendedVigenere.decrypt(key_subs,decrypt)
+    def self.decrypt1(key_trans,key_subs,ciphertext)
+        ciphertext_length = ciphertext.length
+        length_col = ciphertext_length/key_trans
+        if(ciphertext_length%key_trans != 0)
+            length_col += 1
+        end
+        ret = ""
+        decrease = 0;
+        for column_ind in 0..(length_col-1) do
+            for row_ind in 0..(key_trans-1) do
+                pos = length_col*row_ind + column_ind
+                if(column_ind*key_trans + row_ind >= ciphertext_length)
+                    decrease += 1
+                else
+                    ret += ciphertext[pos-decrease]
+                end
+            end
+        end
+        decrypt1_letter = Cipher::ExtendedVigenere.decrypt(key_subs,ret)
+        return decrypt1_letter
     end
-
 end
