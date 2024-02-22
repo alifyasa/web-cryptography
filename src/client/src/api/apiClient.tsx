29199855468b6d0f1c1cc6b4c0b9503c1a5da444
FOS-Intern <intern@fata-organa.com>
2024-02-22 14:27:14 +0700

    add all form

--- src/client/src/api/apiClient.tsx
+++ src/client/src/api/apiClient.tsx
@@ -1,10 +1,30 @@
 import axios from "axios";
 import React from "react";
   
-  export const axiosClient = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BACKEND_API_URL}`,
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  });
+export const axiosClient = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_API_URL}`,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export type EncryptRequest = {
+  cipher: string,
+  data: string
+  key: string
+};
+
+export type DecryptRequest = {
+  cipher: string,
+  data: string
+  key: string
+};
+
+export const postEncrypt = async (parameter: EncryptRequest) => {
+  return await axiosClient.post("/encrypt", parameter);
+};
+
+export const postDecrypt = async (parameter: DecryptRequest) => {
+  return await axiosClient.post("/decrypt", parameter);
+};
--- src/client/src/components/Form.tsx
+++ src/client/src/components/Form.tsx
@@ -1,36 +1,139 @@
 import React, { useState } from 'react';
-// import CryptoJS from 'crypto-js';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css'; // Import default dropdown styles
+import './style/Form.css'; // Import custom form styles
+import { postEncrypt,postDecrypt } from '../api/apiClient';
+import { Cipher } from 'crypto';
 
 const Form: React.FC = () => {
   const [inputText, setInputText] = useState('');
   const [key, setKey] = useState('');
-  const [encryptedText, setEncryptedText] = useState('');
+  const [optionValueType, setOptionValueType] = useState('Text');
+  const [cipherType, setCipherType] = useState("Vigenere");
+  const [showResult, setShowResult] = useState(false);
+  const [resultText, setResultText] = useState("");
+  const [isEncrypt, setEncrypt] = useState("Encrypt");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // const encrypted = CryptoJS.AES.encrypt(inputText, key).toString();
-    // setEncryptedText(encrypted);
+    // Handle form submission here
   };
 
+  const options = ['Text'];
+  const operations = ['Encrypt',
+                      'Decrypt']
+  const cipherList = ['Vigenere',
+                      'Playfair',
+                      'Hill',
+                      'Vigenere_Extend',
+                      'Autokey_Vigenere',
+                      'Affine',
+                      'Super']
+
+  const handleDropdownChange = (option: any) => {
+    setOptionValueType(option.value);
+  };
+
+  const handleDropdownEncrypt = (option: any) => {
+    setEncrypt(option.value);
+  }
+
+  const handleDropdownCipherChange = (option: any) => {
+    setCipherType(option.value)
+  }
+
+  const getOperation = async () => {
+    var request = {
+                      cipher: cipherType,
+                      data: inputText,
+                      key: key
+                  }
+    var response = null
+    if(isEncrypt=="Encrypt") response = await postEncrypt(request);
+    else response = await postDecrypt(request);
+    setShowResult(true);
+    setMessage(response.data.message);
+    setResultText(response.data.data);
+  }
+
   return (
-    <div>
-      <h2>AES Encryption</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="inputText">Input Text:</label><br />
-          <textarea id="inputText" value={inputText} onChange={(e) => setInputText(e.target.value)} rows={4} cols={50}></textarea>
+    <div className="form-container">
+      <h2 className="form-title">Web Cryptography</h2>
+      <form className="encryption-form" onSubmit={handleSubmit}>
+          {showResult && (
+            <div className="encrypted-text-container">
+              <p className="encrypted-text">{message}</p>
+            </div>
+          )}
+        <div className="form-group">
+          <label className="form-label" htmlFor="inputText">Input Type:</label>
+          <Dropdown
+            className="dropdown"
+            options={options}
+            onChange={handleDropdownChange}
+            value={optionValueType}
+            placeholder="Select Input Type"
+          />
+          <label className="form-label" htmlFor="inputText">Encrypt/Decrypt:</label>
+          <Dropdown
+            className="dropdown"
+            options={operations}
+            onChange={handleDropdownEncrypt}
+            value={isEncrypt}
+            placeholder="Select Enrypt or Decrypt!"
+          />
         </div>
-        <div>
-          <label htmlFor="key">Encryption Key:</label><br />
-          <input type="text" id="key" value={key} onChange={(e) => setKey(e.target.value)} />
+        <div className="form-group">
+          <label className="form-label" htmlFor="inputText">Cipher:</label>
+          <Dropdown
+            className="dropdown"
+            options={cipherList}
+            onChange={handleDropdownCipherChange}
+            value={cipherType}
+            placeholder="Select Cipher Type"
+          />
         </div>
-        <div>
-          <button type="submit">Encrypt</button>
+        <div className="form-group">
+          <label className="form-label" htmlFor="inputText">Input Text:</label>
+          <textarea
+            className="form-textarea"
+            id="inputText"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            rows={4}
+            cols={50}
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="key">{isEncrypt} Key:</label>
+          <input
+            className="form-input"
+            type="text"
+            id="key"
+            value={key}
+            onChange={(e) => setKey(e.target.value)}
+          />
+          {
+            cipherType == "Affine" && 
+            <div>
+              Key is in format a,b. each of them are integer
+            </div>
+          }
+          {
+            cipherType == "Super" && 
+            <div>
+              Key is in format a,b. a is string and b is integer
+            </div>
+          }
+        </div>
+        <div className="form-group">
+          <button className="form-button" type="submit" onClick={getOperation}>{isEncrypt}</button>
         </div>
       </form>
-      {encryptedText && (
-        <div>
-          <p>Encrypted Text: {encryptedText}</p>
+      {showResult && (
+        <div className="encrypted-text-container">
+          <p className="encrypted-text">Result: {resultText}</p>
         </div>
       )}
     </div>
